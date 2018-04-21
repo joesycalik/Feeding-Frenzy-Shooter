8e02e2f7e55f3bea7378e9b1357c410fc73c01c0
Joe Sycalik <sycalik@gmail.com>
2018-04-21 01:13:41 -0400

    Base Player Movement Added
    -Player can move up, down, left, and right
    -Player rotates to face the mouse
    -Camera follows the player

---
+++ Feeding Frenzy Shooter/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+
+    private void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        transform.Translate(x * speed * Time.deltaTime, y * speed * Time.deltaTime, 0, Space.World);
+
+        // convert mouse position into world coordinates
+        Vector2 mouseScreenPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        // get direction you want to point at
+        Vector2 direction = (mouseScreenPosition - (Vector2)transform.position).normalized;
+
+        // set vector of transform directly
+        transform.up = direction;
+    }
+}
